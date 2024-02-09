5b8937bac4be3aee1e6920f9507e59a77bf47658
Thibault <thibault_paillon@live.fr>
2024-02-09 11:38:15 +0100

    Updated the Case Converter to handle script & style properly in their own file

---
+++ Case Converter/script.js
@@ -0,0 +1,47 @@
+document.getElementById("upper-case").addEventListener("click", function () {
+    let words = document.getElementById("input").value.toUpperCase();
+    document.getElementById("input").value = words;
+});
+
+document.getElementById("lower-case").addEventListener("click", function () {
+    let words = document.getElementById("input").value.toLowerCase();
+    document.getElementById("input").value = words;
+});
+
+function properCase(element){
+    return element.toLowerCase().replace(/^(.)|\s(.)/g,
+        function($1) { return $1.toUpperCase(); });
+}
+
+document.getElementById("proper-case").addEventListener("click", function() {
+    let element = document.querySelector("textarea");
+    element.value = properCase(element.value);
+});
+
+function sentenceCase(element){
+    return element.toLowerCase().replace(/^(.)|\.(\s)*(.)/g,
+        function ($1) {return $1.toUpperCase();});
+}
+
+document.getElementById("sentence-case").addEventListener("click", function() {
+    let element = document.querySelector("textarea");
+    element.value = sentenceCase(element.value);
+});
+
+function download(filename, text) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+document.getElementById("save-text-file").addEventListener("click", function() {
+    let text = document.querySelector("textarea").value;
+    let filename = "text.txt"
+    download(filename ,document.querySelector("textarea").value);
+}, false);
